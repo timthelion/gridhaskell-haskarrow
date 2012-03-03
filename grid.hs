479e9c7f9f61a1727f1cc04bc700509ba5543c27
Timothy Hobbs <timothyhobbs@seznam.cz>
2012-03-03 18:30:09 +0100

    Got Value paths working.

--- grid.hs
+++ grid.hs
@@ -7,6 +7,22 @@ imports_code = "\n"
 pure_code = "\n"
 main_code = "main = " ++ (function_code 0 0) ++ "\n"
 
+cells = 
+	(Action (0,0) "getChar" False True False None 
+	(Action (1,0) "'n'" True True False (SteppingStone (1,1) 
+										(SteppingStone (2,1)
+										(SteppingStone (3,1)
+										(SteppingStone (4,1)
+										(PathDestination (4,0))))))
+	(Action (2,0) "(==)" True True True None
+	(If (3,0)
+	(Destination (4,0) (1,0) None 
+	(Action (5,0) "putChar" False False True None End))
+	(End)))))
+
+main = do writeFile file_name code
+
+code = licence_code++imports_code++pure_code++main_code++(actions_code cells [] 0)
 
 type Push = Bool
 type Pull = Bool
@@ -14,20 +30,18 @@ type Then = Cell
 type Else = Cell
 type Origin = Point
 type Return = Bool
-data Cell = Action Point String Return Push Pull ValuePath Cell
-		  | Destination Origin Cell
+data Cell = Action Point String Return Push Pull Path Cell
+		  | Destination Point Origin Path Cell
 		  | If Point Then Else
-		  | Jump ActionPath
+		  | Jump Path
 		  | End
+data Path = SteppingStone Point Path
+		  	   | PathDestination Point
+		  	   | None
 
-cells = 
-	(Action (0,0) "getChar" False True False None 
-	(Action (1,0) "'n'" True True False None
-	(Action (2,0) "(==)" True True True None
-	(If (3,0)
-	(Action (4,0) "'n'" True True False None 
-	(Action (5,0) "putChar" False False True None End))
-	(End)))))
+type X = Int
+type Y = Int
+type Point = (X, Y)
 
 point (Action (x,y) _ _ _ _ _ _) = (x, y)
 point (If (x,y) _ _) = (x, y)
@@ -52,6 +66,8 @@ if_body End _ = "return ()"
 if_body (Action (x, y) _ _ _ _ _ _) values =
 	function_code x y ++
 	(value_codes values)
+if_body (Destination mypoint _ _ _) values =
+	if_body (Action mypoint undefined undefined undefined undefined undefined undefined) values
 
 body_code code preturn stack =
 	if preturn
@@ -78,25 +94,30 @@ function_tail next values stack =
 		"\n" ++
 		(actions_code next values stack)
 
+add_path values None _ _ =
+	values
+add_path values _ x y =
+	(x,y):values
+
 actions_code :: Cell -> [Point] -> Int -> [Char]
 
-actions_code (Action (x, y) code preturn True False _ next) values stack = 
+actions_code (Action (x, y) code preturn True False path next) values stack = 
 	function_header x y values stack ++
 	(body_code code preturn 0) ++
-	bind x y values stack next ++
-	function_tail next values (stack + 1)
+	bind x y (add_path values path x y) stack next ++
+	function_tail next (add_path values path x y) (stack + 1)
 	
-actions_code (Action (x, y) code preturn False True _ next) values stack =
+actions_code (Action (x, y) code preturn False True path next) values stack =
 	function_header x y values stack ++
 	(body_code code preturn stack)++
-	bind x y values 0 next ++
-	function_tail next values 0
+	bind x y (add_path values path x y) 0 next ++
+	function_tail next (add_path values path x y) 0
 
-actions_code (Action (x, y) code preturn True True _ next) values stack =
+actions_code (Action (x, y) code preturn True True path next) values stack =
 	function_header x y values stack ++
 	(body_code code preturn stack)++
-	bind x y values 0 next ++
-	function_tail next values 1
+	bind x y (add_path values path x y) 0 next ++
+	function_tail next (add_path values path x y) 1
 
 actions_code (If (x, y) a_then a_else) values 1 =
 	function_header x y values 1 ++
@@ -108,24 +129,14 @@ actions_code (If (x, y) a_then a_else) values 1 =
 	(if_body a_else values) ++
 	function_tail a_then values 0 ++
 	function_tail a_else values 0 
+
+actions_code (Destination mypoint (xo,yo) path next) values stack =
+	actions_code (Action mypoint code False True False path next) values stack
+	where code = "return "++(value_code xo yo)
 	
+
 actions_code (End) _ _ = ""
 
-destinations (ValueSteppingStone _ path) = destinations path
-destinations (ValuePathFork _ path1 path2) = destinations path1 ++ destinations path2
-destinations (ValuePathDestination point) = [point]
-data ValuePath = ValueSteppingStone Point ValuePath
-		  	   | ValuePathFork Point ValuePath ValuePath
-		  	   | ValuePathDestination Point
-		  	   | None
-
-data ActionPath = ActionSteppingStone Point ActionPath
-				| ActionPathDestination Point
-
-type X = Int
-type Y = Int
-type Point = (X, Y)
-
-main = do writeFile file_name code
-
-code = licence_code++imports_code++pure_code++main_code++(actions_code cells [] 0)
+destinations (SteppingStone _ path) = destinations path
+--destinations (PathFork _ path1 path2) = destinations path1 ++ destinations path2
+destinations (PathDestination point) = [point]
